c8fc755fb9a023fc9757dd9b2fdb2394eca79e2a
bcygsws <bcygswspring168@gmail.com>
2025-07-17 00:02:00 +0800

    :memo: RN项目文档终极二

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -62,6 +62,7 @@ const Main = () => {
     </Tab.Navigator>) : (<>{navigation.navigate('Login')}</>);
 };
 
+
 // const styles = StyleSheet.create({
 //     homeContainer: {
 //         alignItems: 'center',
